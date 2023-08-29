be7c9a0dbe5cd77c4a32e64e3b13764f4da9d841
John Carges <johncarges@gmail.com>
2023-08-29 11:53:49 -0400

    passes all tests

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    'Mr. Chocolate', 10, 'm', 'brown', 'calm', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    'Rowdy', 10, 'm', 'brown', 'calm', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    'Tabitha', 10, 'f', 'brown', 'calm', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    'Sergeant Brown', 10, 'm', 'brown', 'calm', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    'Melissa', 10, 'f', 'brown', 'calm', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    'Grinch', 10, 'm', 'brown', 'calm', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    'Wendy', 10, 'f', 'brown', 'calm', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    NULL, 10, 'm', 'brown', 'calm', True);
